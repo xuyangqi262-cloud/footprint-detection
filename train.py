5f11f9a40b5864325ea3ac50e5638ebd942ed12d
xuyangqi262-cloud <xuyangqi262@gmail.com>
2026-04-23 10:17:18 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,1 +1,11 @@
-print("train start")
+import torch
+from model import FootprintModel
+
+def train():
+    print("Footprint training start")
+
+    model = FootprintModel()
+    print(model)
+
+if __name__ == "__main__":
+    train()
