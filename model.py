4ac11eb6a2daaa97d97ee2cecf394797ad0ef211
xuyangqi262-cloud <xuyangqi262@gmail.com>
2026-04-23 10:17:54 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,2 +1,14 @@
-class Model:
-    pass
+import torch.nn as nn
+
+class FootprintModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(10, 32),
+            nn.ReLU(),
+            nn.Linear(32, 2)
+        )
+
+    def forward(self, x):
+        return self.net(x)
